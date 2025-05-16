f1f67d54cdd3e054d87d798525b5bb1a320e2b0f
source-horse <rohitpc03@gmail.com>
2025-05-16 18:56:01 +0530

    vs

---
+++ product.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+
+const productSchema = new mongoose.Schema({
+  name: String,
+  price: Number,
+  description: String,
+  imageUrl: String,
+  category: String,
+  stock: Number,
+});
+
+module.exports = mongoose.model('Product', productSchema);
